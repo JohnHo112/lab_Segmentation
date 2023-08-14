b2c1d77838645d31492292db40cc915751657b35
JohnHo112 <hokengseng0112@gmail.com>
2023-08-14 16:59:01 +0800

    8/14 debug new_center

--- Kmeans/src/Kmean_adv.py
+++ Kmeans/src/Kmean_adv.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from scipy import ndimage
 from skimage import measure
-import math
+import multiprocessing
 import numpy as np
 import random
 import time
@@ -20,6 +20,36 @@ def image_mark(image, points, label):
     plt.legend()
     plt.figure()
 
+def show_region(region):
+    r = []
+    for i in region:
+        r.append(i)
+    return r
+
+
+def show_segamented_image(image, region, l):
+    fig, ax = plt.subplots(4, 4)
+    i = 0
+    j = 0
+    for k in range(16):
+        if i // 4 == 1:
+            i = 0
+            j += 1
+        temp = image.copy()
+        for m, n in region[l[k]]["index"]:
+            temp[m, n] = 255
+        ax[i][j].imshow(temp)
+        i += 1
+
+def sort_region(region):
+    a = []
+    for i in region:
+        a.append((len(region[i]["index"]), i))
+    a = sorted(a, reverse=True)
+    sorted_region = {}
+    for l, i in a:
+        sorted_region[i] = region[i]
+    return sorted_region
 
 def Kmean(image, K, l1, l2, L, iter):
     M, N, O = image.shape
@@ -53,7 +83,7 @@ def Kmean(image, K, l1, l2, L, iter):
         min_g = g[k[0], k[1]]
         for i in range(k[0]-L, k[0]+L+1):
             for j in range(k[1]-L, k[1]+L+1):
-                if i < 0 or j < 0 or i > N-1 or j > M-1:
+                if i < 0 or j < 0 or i > M-1 or j > N-1:
                     continue
                 elif g[i, j] < min_g:
                     min_g = g[i, j]
@@ -61,30 +91,22 @@ def Kmean(image, K, l1, l2, L, iter):
         return min_g_index
     
     def set_all_min_gradient_points(K_points, g, L):
-        new_K_points = []
-        for k in K_points:
-            if find_min_gradient_point(k, g, L) in new_K_points or find_min_gradient_point(k, g, L) in K_points:
-                new_K_points.append(k)
-                continue
-            else:
-                new_K_points.append(find_min_gradient_point(k, g, L))
-        # print(f"new K_points: {new_K_points}")
-        return new_K_points
+        for i, k in enumerate(K_points):
+            K_points[i] = find_min_gradient_point(k, g, L)
+        return K_points
             
+
     # Calculate each point distance with init points
     def calculate_distance(m, n, regions):
-        distances = []       
-        for k in regions:
-            if m == regions[k]["index"][0][0] and n == regions[k]["index"][0][1]:
-                return 
+        distances = []     
         for k in regions: 
             distances.append((l1*((m-regions[k]["index"][0][0])**2+(n-regions[k]["index"][0][1])**2)+l2*(y[m, n]-regions[k]["y"])**2+(cb[m, n]-regions[k]["cb"])**2+(cr[m, n]-regions[k]["cr"])**2)**(1/2))
         regions[np.argmin(distances)]["index"].append((m, n))
 
     # After finishing all points distance, calculate new center point from mean value
     def new_center(regions):
-        new_m, new_n, new_y, new_cr, new_cb = 0, 0, 0, 0, 0
         for k in regions:
+            new_m, new_n, new_y, new_cr, new_cb = 0, 0, 0, 0, 0
             for m, n in regions[k]["index"]:
                 new_m += m
                 new_n += n
@@ -97,7 +119,7 @@ def Kmean(image, K, l1, l2, L, iter):
             new_y = new_y/len(regions[k]["index"])
             new_cb = new_cb/len(regions[k]["index"])
             new_cr = new_cr/len(regions[k]["index"])
-            regions[k] = {"y": new_y, "cb": new_cb, "cr": new_cr, "index": [(int(math.floor(new_m)), int(math.floor(new_n)))]}
+            regions[k] = {"y": new_y, "cb": new_cb, "cr": new_cr, "index": [(int(new_m), int(new_n))]}
 
     # Making the regions map in R matrix
     def regions_map(regions, R):
@@ -115,32 +137,7 @@ def Kmean(image, K, l1, l2, L, iter):
         for m in range(M):
             for n in range(N):
                 regions[R[m, n]]["index"].append((m, n))
-        return R, regions
-    
-    def test_total_points(regions):
-        total = 0
-        for r in regions:
-            total += len(regions[r]["index"])
-        print(f"total: {total}")
-
-    def test_find_repeat(regions):
-        temp = []
-        repeat = []
-        for r in regions:
-            for i in regions[r]["index"]:
-                if i in temp:
-                    repeat.append(i)
-                temp.append(i)
-        print(f"repeat num: {len(repeat)}")
-        print(f"repeat: {repeat}")
-    
-    def test_repeat_points(points):
-        for i in range(len(points)-1):
-            for j in range(i+1, len(points)):
-                if points[i] == points[j]:
-                    print(True)
-            
-
+        return R, regions       
 
     # The main function in kmean function        
     def run():
@@ -154,56 +151,22 @@ def Kmean(image, K, l1, l2, L, iter):
         regions = {}
         for k in range(K):
             regions[k] = {"y": y[K_points[k]], "cb": cb[K_points[k]], "cr": cr[K_points[k]], "index": [K_points[k]]}
-            
-        # print(regions)
+    
         for i in range(iter):   
-            test_total_points(regions)
-            test_find_repeat(regions)
+            print(f"iter: {i}")
             for m in range(M):
                 for n in range(N):
                     calculate_distance(m, n, regions)
-            test_total_points(regions)
-            test_find_repeat(regions)
             if i == iter-1:
                 break
             else:
-                new_center(regions)   
+                new_center(regions)
+
         regions_map(regions, R)
         R, regions = separet_region(R)
         return R, regions
     
     return run()
-                
-def show_region(region):
-    r = []
-    for i in region:
-        r.append(i)
-    return r
-
-
-def show_segamented_image(image, region, l):
-    fig, ax = plt.subplots(4, 4)
-    i = 0
-    j = 0
-    for k in range(16):
-        if i // 4 == 1:
-            i = 0
-            j += 1
-        temp = image.copy()
-        for m, n in region[l[k]]["index"]:
-            temp[m, n] = 255
-        ax[i][j].imshow(temp)
-        i += 1
-
-def sort_region(region):
-    a = []
-    for i in region:
-        a.append((len(region[i]["index"]), i))
-    a = sorted(a, reverse=True)
-    sorted_region = {}
-    for l, i in a:
-        sorted_region[i] = region[i]
-    return sorted_region
 
 # read image
 path = "Pic/Lena_256bmp.bmp"
@@ -211,7 +174,7 @@ image = plt.imread(path)
 
 # main function
 start = time.time()
-R, regions = Kmean(image, 200, 0.6, 0.8, 10, 10)
+R, regions = Kmean(image, 200, 0.6, 0.8, 3, 10)
 end = time.time()
 print(f"time: {end- start}")
 
